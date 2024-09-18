719c5b89345edc15f8dbc6f3e21fd08fb1a5dc81
vokx7 <vokx77@gmail.com>
2024-09-18 10:25:36 +0200

    start

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20">
+      <main className="flex flex-col items-center"></main>
+    </div>
+  );
+}
